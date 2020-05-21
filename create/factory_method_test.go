8a933a76e0ea95460e67604756e8e82d88b6e774
于海洋 <yhy@iothinking.com>
2020-05-21 11:41:19 +0800

    完成了工厂模式相关的代码

---
+++ create/factory_method_test.go
@@ -0,0 +1,12 @@
+package create
+
+import (
+	"testing"
+)
+
+func TestNilDpFactory_NewDp(t *testing.T) {
+	// 通过不同的 factory 来获取扩展获取不同的类型的 dp 点
+	//var factory DpFactory
+	//nilDp, _ := NilDpFactory.NewDp(10, "")
+	//fileDp, _ := FileDpFactory.NewDp(11, "www.iothink.com/a.json")
+}
