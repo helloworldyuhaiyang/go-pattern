815f3654535327e9b3ce700db691080e52e92813
于海洋 <yhy@iothinking.com>
2020-06-03 16:32:08 +0800

    远程代理

---
+++ structural/remote_proxy_test.go
@@ -0,0 +1,36 @@
+package structural
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"testing"
+)
+
+func Test_Server(t *testing.T) {
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	http.Serve(l, nil)
+}
+
+func Test_Client(t *testing.T) {
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	// Synchronous call
+	args := &Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error:", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
+}
